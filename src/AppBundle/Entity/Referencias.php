8c82852d2c01b2b478a43b597dbe376de8ed500d
Julian Pasquale <julianagustin78@gmail.com>
2018-05-19 19:05:35 -0300

    DocumentTypeJsonParser class removed

--- src/AppBundle/Entity/Referencias.php
+++ src/AppBundle/Entity/Referencias.php
@@ -11,7 +11,7 @@ class Referencias {
 
     public function __construct() {}
 
-    public static function fromArray($attributes) {
+    private function fromArray($attributes) {
         $instance = new self();
         $instance->setId($attributes['id']);
         $instance->setName($attributes['nombre']);
@@ -36,53 +36,48 @@ class Referencias {
 
     public static function documentType($value=''){
     	if ($value!='') {
-    		return DocumentTypeJsonParser::single(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/tipo-documento/".$value));
+    		return $this->single(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/tipo-documento/".$value));
     	}
-    	return DocumentTypeJsonParser::fromCollection(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/tipo-documento"));
+    	return $this->fromCollection(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/tipo-documento"));
     }
 
     public static function obraSocial($value=''){
     	if ($value!='') {
-    		return DocumentTypeJsonParser::single(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/obra-social/".$value));
+    		return $this->single(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/obra-social/".$value));
     	}
-    	return DocumentTypeJsonParser::fromCollection(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/obra-social"));
+    	return $this->fromCollection(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/obra-social"));
     }
 
 	public static function tipoAgua($value=''){
     	if ($value!='') {
-    		return DocumentTypeJsonParser::single(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/tipo-agua/".$value));
+    		return $this->single(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/tipo-agua/".$value));
     	}
-    	return DocumentTypeJsonParser::fromCollection(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/tipo-agua"));
+    	return $this->fromCollection(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/tipo-agua"));
     }
 
 	public static function tipoCalefaccion($value=''){
     	if ($value!='') {
-    		return DocumentTypeJsonParser::single(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/tipo-calefaccion/".$value));
+    		return $this->single(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/tipo-calefaccion/".$value));
     	}
-    	return DocumentTypeJsonParser::fromCollection(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/tipo-calefaccion"));
+    	return $this->fromCollection(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/tipo-calefaccion"));
     }
 
 	public static function tipoVivienda($value=''){
     	if ($value!='') {
-    		return DocumentTypeJsonParser::single(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/tipo-vivienda/".$value));
+    		return $this->single(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/tipo-vivienda/".$value));
     	}
-    	return DocumentTypeJsonParser::fromCollection(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/tipo-vivienda"));
+    	return $this->fromCollection(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/tipo-vivienda"));
     }
-}
-
-class DocumentTypeJsonParser {
-
-    private function __construct() {}
-
-    public static function single($json) {
-        return Referencias::fromArray(json_decode($json, true));
+    
+    private function single($json) {
+        return $this->fromArray(json_decode($json, true));
     }
-
+    
     public static function fromCollection($json_collection) {
         $data = json_decode($json_collection, true);
 
         foreach($data as $single_json) {
-          $array[]= Referencias::fromArray($single_json);
+          $array[]= $this->fromArray($single_json);
         }
         return $array;
     }
