ce402658b4a8ee86d9134bc96084a452f7f52d23
Nacho Soloaga <nachosoloaga@gmail.com>
2018-05-20 16:18:29 -0300

    Refactor de html y css de sobre el hospital

--- src/AppBundle/Entity/Referencias.php
+++ src/AppBundle/Entity/Referencias.php
@@ -36,48 +36,48 @@ class Referencias {
 
     public static function documentType($value=''){
     	if ($value!='') {
-    		return $this->single(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/tipo-documento/".$value));
+    		return Referencias::single(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/tipo-documento/".$value));
     	}
-    	return $this->fromCollection(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/tipo-documento"));
+    	return Referencias::fromCollection(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/tipo-documento"));
     }
 
     public static function obraSocial($value=''){
     	if ($value!='') {
-    		return $this->single(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/obra-social/".$value));
+    		return Referencias::single(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/obra-social/".$value));
     	}
-    	return $this->fromCollection(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/obra-social"));
+    	return Referencias::fromCollection(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/obra-social"));
     }
 
 	public static function tipoAgua($value=''){
     	if ($value!='') {
-    		return $this->single(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/tipo-agua/".$value));
+    		return Referencias::single(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/tipo-agua/".$value));
     	}
-    	return $this->fromCollection(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/tipo-agua"));
+    	return Referencias::fromCollection(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/tipo-agua"));
     }
 
 	public static function tipoCalefaccion($value=''){
     	if ($value!='') {
-    		return $this->single(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/tipo-calefaccion/".$value));
+    		return Referencias::single(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/tipo-calefaccion/".$value));
     	}
-    	return $this->fromCollection(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/tipo-calefaccion"));
+    	return Referencias::fromCollection(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/tipo-calefaccion"));
     }
 
 	public static function tipoVivienda($value=''){
     	if ($value!='') {
-    		return $this->single(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/tipo-vivienda/".$value));
+    		return Referencias::single(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/tipo-vivienda/".$value));
     	}
-    	return $this->fromCollection(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/tipo-vivienda"));
+    	return Referencias::fromCollection(file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/tipo-vivienda"));
     }
     
     private function single($json) {
-        return $this->fromArray(json_decode($json, true));
+        return Referencias::fromArray(json_decode($json, true));
     }
     
-    public static function fromCollection($json_collection) {
+    private function fromCollection($json_collection) {
         $data = json_decode($json_collection, true);
 
         foreach($data as $single_json) {
-          $array[]= $this->fromArray($single_json);
+          $array[]= Referencias::fromArray($single_json);
         }
         return $array;
     }
