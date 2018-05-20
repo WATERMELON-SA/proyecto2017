2e0d44c1fff1f7e1c7a135f23c63aa57ac10b0af
Julian Pasquale <julianagustin78@gmail.com>
2018-05-20 18:31:50 -0300

    config para heroku

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -3,7 +3,7 @@
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
-use Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle;
+use Doctrine\Bundle\FixturesBundle;
 
 class AppKernel extends Kernel
 {
