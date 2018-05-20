104cf17c2b07d98a1c52ee702b1b2618e520fbda
Julian Pasquale <julianagustin78@gmail.com>
2018-05-20 19:21:07 -0300

    Procfile for Apache and PHP

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
-            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle()
         ];
        
         if (in_array($this->getEnvironment(), ['dev', 'test','prod'], true)) {
@@ -28,6 +27,7 @@ class AppKernel extends Kernel
             if ('dev' === $this->getEnvironment()) {
                 $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
                 $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
+                $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
             }
         }
 
