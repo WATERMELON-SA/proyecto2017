ec64a55d1a4e7947f528ee2365d086a4d205cfe2
Julian Pasquale <julianagustin78@gmail.com>
2018-05-20 15:40:14 -0300

    database driver in parameters.yml and fixtures working

--- src/AppBundle/DataFixtures/AppFixtures.php
+++ src/AppBundle/DataFixtures/AppFixtures.php
@@ -11,12 +11,13 @@ class AppFixtures implements ORMFixtureInterface
     public function load(ObjectManager $manager)
     {
         for ($i = 0; $i < 20; $i++) {
+            date_default_timezone_set('UTC');
             $pacient = new Pacient();
             $pacient->setName('paciente '.$i);
             $pacient->setSurname('surname'.$i);
-            $pacient->setAdress('calle'.$i);
-            $pacient->setPhone("($i*100000)");
-            $pacient->setBirthdate(new Date.today);
+            $pacient->setAddress('calle'.$i);
+            $pacient->setPhone((String) $i*100000);
+            $pacient->setBirthdate(new \DateTime('today'));
             $pacient->setGender('1');
             $pacient->setDniNumber($i*100050);
             $pacient->setDeleted(false);
