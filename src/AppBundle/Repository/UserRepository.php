21a1c91cda502e12280555e4385f5d8aff9a5934
Julian Pasquale <julianagustin78@gmail.com>
2018-05-26 13:32:08 -0300

    probando queries en UserRepository

--- src/AppBundle/Repository/UserRepository.php
+++ src/AppBundle/Repository/UserRepository.php
@@ -21,28 +21,31 @@ class UserRepository extends \Doctrine\ORM\EntityRepository{
 
 	public function getUsers($cantPags,$busqueda,$activo,$pagactual=1,$username){
 		$inicio=((int)$pagactual-1)*$cantPags;
-		if (($busqueda!='') or ($activo!='')) {
-			$query = $this->createQueryBuilder('user u')
-    		->where("u.active=:activo and u.deleted= :deleted and u.username!= :username and (u.username LIKE :busqueda OR u.name LIKE :busqueda OR u.surname LIKE :busqueda OR u.email LIKE :busqueda)")
-    		->setParameter('activo', $activo)
-    		->setParameter('deleted', false)
-    		->setParameter('username', $username) 
-    		->setParameter('busqueda', '%$busqueda%')
-    		->setFirstResult($inicio)
-    		->setMaxResults($cantPags)
-    		->getQuery();
+		
+		$query = 
+			$this->createQueryBuilder('user')
+			->where("user.deleted= :deleted and user.username!= :username")
+			->setParameter('deleted', false)
+			->setParameter('username', $username);
+    		
+		if ($activo != ''){
+			$query = 
+				$query
+					->andWhere('user.active= :activo')
+    				->setParameter('activo', true);
 		}
-		else{
-			$query = $this->createQueryBuilder('user')
-    		->where("user.deleted= :deleted and user.username!= :username")
-    		->setParameter('deleted', false)
-    		->setParameter('username', $username)
-    		->setFirstResult($inicio)
-    		->setMaxResults($cantPags)
-    		->getQuery();
+		
+		if ($busqueda != ''){
+			$query = 
+				$query
+					->andWhere('user.username LIKE :busqueda OR user.name LIKE :busqueda OR user.surname LIKE :busqueda OR user.email LIKE :busqueda')	
+		    		->setParameter('busqueda', "%$busqueda%");
 		}
-		$usuarios=$query->getResult();
-		return $usuarios;
+
+		return $query->setFirstResult($inicio)
+    		->setMaxResults($cantPags)
+    		->getQuery()
+    		->getResult();
 	}
 
 }
